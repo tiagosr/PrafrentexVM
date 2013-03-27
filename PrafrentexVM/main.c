b43a415618e3a0962ca3c680854f0e2d5992dbd0
Tiago Rezende <tiagosr@gmail.com>
2013-03-27 18:19:53 -0300

    Initial Commit

---
+++ PrafrentexVM/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  PrafrentexVM
+//
+//  Created by Café Artes Visuais on 27/03/13.
+//  Copyright (c) 2013 Café Artes Visuais. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
